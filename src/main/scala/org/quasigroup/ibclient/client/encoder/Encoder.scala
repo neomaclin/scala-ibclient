b4daba0856b2fe96f18066221cb75dd3c868bdd0
Neo <neomac.lin@gmail.com>
2023-03-14 00:26:27 -0400

    adding mirroOf for encoders

--- src/main/scala/org/quasigroup/ibclient/client/encoder/Encoder.scala
+++ src/main/scala/org/quasigroup/ibclient/client/encoder/Encoder.scala
@@ -3,7 +3,9 @@ package org.quasigroup.ibclient.client.encoder
 import fs2.Chunk
 import scodec.bits.Literals.Utf8
 
+import scala.collection.mutable
 import scala.collection.mutable.Buffer
+import scala.compiletime.summonFrom
 import scala.deriving.Mirror
 
 object Encoder {
@@ -30,13 +32,30 @@ object Encoder {
 
   given encodeList[T](using e:Encoder[T]): Encoder[List[T]] = _.foldLeft(Buffer.empty)(_ ++ e(_))
 
-  // inline def derive[T](m: Mirror.Of[T]): Encoder[T] =
-  //   def deriveSum(s.)
-  //   lazy val instances = summonAll[m.MirroredElemTypes]
-  //   inline m match
-  //     case s: Mirror.SumOf[T] => deriveSum(s, instances)
-  //     case p: Mirror.Product[T] => deriveProduct(p, instances)
-    
+  import scala.compiletime.{erasedValue, summonInline}
+
+  def encoderSum[T](s: Mirror.SumOf[T], encoders: => List[Encoder[_]]): Encoder[T] = new Encoder[T]:
+      def apply(t: T): Buffer[Byte] = {
+        val index = s.ordinal(t) // (2)
+        encoders(index).asInstanceOf[Encoder[Any]](t) // (3)
+      }
+  def encoderProduct[T](p: Mirror.ProductOf[T], encoders: => List[Encoder[_]]): Encoder[T] = new Encoder[T]:
+      def apply(t: T): Buffer[Byte] = {
+        t.asInstanceOf[Product].productIterator.zip(encoders.iterator)
+          .map((p, e) => e.asInstanceOf[Encoder[Any]](p)).foldLeft(Buffer.empty)(_ ++ _)
+      }
+  inline def summonAll[T <: Tuple]: List[Encoder[_]] =
+    inline erasedValue[T] match
+      case _: EmptyTuple => Nil
+      case _: (t *: ts) => summonInline[Encoder[t]] :: summonAll[ts]
+
+  inline given derived[T](using m: Mirror.Of[T]): Encoder[T] =
+    lazy val encoders = summonAll[m.MirroredElemTypes]
+    inline m match
+      case s: Mirror.SumOf[T] => encoderSum(s, encoders)
+      case p: Mirror.ProductOf[T] => encoderProduct(p, encoders)
+
+
 }
 
 trait Encoder[A] { self =>
